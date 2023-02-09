20c298836c382a935bd315aa364392818aa77e80
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-02-08 22:58:03 -0500

    Update 2_analysis.py

--- pages/2_analysis.py
+++ pages/2_analysis.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Hufty Bikes Analysis App", layout="wide")
 st.title("Analyzing of Hufty Bikes")
+st.markdown(
+        "Hey there! Welcome to Clarison's Hufty Bikes Analysis App. will add despription here ")
