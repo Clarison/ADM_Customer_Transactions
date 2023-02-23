618bd4b78ef0a6bcf8755e7c86a95c9f3ee9219d
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-02-23 12:50:35 -0500

    Update 1_Customer_Demography.py

--- pages/1_Customer_Demography.py
+++ pages/1_Customer_Demography.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 
 df = ge.read_excel(
         "pages/file.xlsx",
-	engine="openpyxl" ,
+	engine="xlrd" ,
         sheet_name="CustomerDemographic"
     )
 
