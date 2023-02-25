c105f52448ad2f6c6117a675394558ca3557e614
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-02-25 10:58:42 -0500

    Update 2_Cohort_analysis.py

--- pages/2_Cohort_analysis.py
+++ pages/2_Cohort_analysis.py
@@ -46,7 +46,7 @@ def load_data():
 
     # Load data
     transaction_df = pd.DataFrame(results, columns=[desc[0] for desc in cur.description])
-    transaction_df.rename(columns=str.lower, inplace=True)
+    transaction_df = transaction_df.rename(columns=str.lower, inplace=True)
     transaction_df['transaction_date'] = pd.to_datetime(df['transaction_date'])
     
     
