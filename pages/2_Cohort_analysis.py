3a8e4334b100df251224330929c187bacb63a2d1
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-02-25 01:13:11 -0500

    Update 2_Cohort_analysis.py

--- pages/2_Cohort_analysis.py
+++ pages/2_Cohort_analysis.py
@@ -1,1 +1,10 @@
-
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.types import IntegerType, StringType, StructType, FloatType, StructField, DateType, Variant
+from snowflake.snowpark.functions import udf, sum, col,array_construct,month,year,call_udf,lit
+from snowflake.snowpark.version import VERSION
+# Misc
+import json
+import pandas as pd
+import logging 
+logger = logging.getLogger("snowflake.snowpark.session")
+logger.setLevel(logging.ERROR)
