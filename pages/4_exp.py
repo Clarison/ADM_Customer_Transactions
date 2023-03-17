564bf6f3139ccc82c37181fc4c56def1f577074d
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-03-17 17:38:36 -0400

    Create 4_exp.py

---
+++ pages/4_exp.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import datetime as dt
+import snowflake.connector as sf
+from sqlalchemy import create_engine
+from snowflake.sqlalchemy import URL
+
+#trying chemy
+engine = create_engine(URL(
+    account = 'wh80921.us-east-2.aws',
+    user = 'clarison',
+    password = '23301631cD*',
+    database = 'SNOWFLAKE_SAMPLE_DATA',
+    schema = 'TPCDS_SF10TCL',
+    warehouse = 'compute_wh'
+))
+
+
+# The code below is for the title and logo for this page.
+st.set_page_config(page_title="TPCDS dataset", page_icon="🚲")
+
+
+st.title("TPCDS dataset")
+
+st.write("")
+
+
+st.write("new")
+df = pd.read_sql_query("SELECT  top 20 C_CURRENT_ADDR_SK FROM customer", engine)
+df.rename(columns=str.lower, inplace=True)
+st.dataframe(df)
+
+
+df2 = pd.read_sql_query("SELECT  top 20 * FROM customer", engine)
+st.dataframe(df2)
