69e8108c9d94b53223429e17a720c06b59e4951b
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-17 21:10:17 -0400

    Update 3_connect_d.py

--- pages/3_connect_d.py
+++ pages/3_connect_d.py
@@ -28,15 +28,6 @@ st.title("TPCDS dataset")
 st.write("")
 
 
-st.write("query 26")
-df = pd.read_sql_query("""select  i_item_id, avg(cs_quantity) agg1,avg(cs_list_price) agg2,avg(cs_coupon_amt) agg3,avg(cs_sales_price) agg4 
-                       from catalog_sales,customer_demographics, date_dim, item, promotion where cs_sold_date_sk = d_date_sk and cs_item_sk = i_item_sk 
-                       and cs_bill_cdemo_sk = cd_demo_sk and cs_promo_sk = p_promo_sk and cd_gender = 'M' and cd_marital_status = 'S' 
-                       and cd_education_status = 'College' and (p_channel_email = 'N' or p_channel_event = 'N') and d_year = 2000
-                       group by i_item_id order by i_item_id limit 100;""", engine)
-df.rename(columns=str.lower, inplace=True)
-st.dataframe(df)
-
 
 
 st.write("For all items sold in stores located in six states during a given year, find the average quantity, average list price,average list sales price, average coupon amount for a given gender, marital status, education and customer demographic")
@@ -111,61 +102,6 @@ st.pyplot(fig)
 
 
 
-st.write("query 28")
-
-df = pd.read_sql_query("""select  *
-from (select avg(ss_list_price) B1_LP
-            ,count(ss_list_price) B1_CNT
-            ,count(distinct ss_list_price) B1_CNTD
-      from store_sales
-      where ss_quantity between 0 and 5
-        and (ss_list_price between 8 and 8+10 
-             or ss_coupon_amt between 459 and 459+1000
-             or ss_wholesale_cost between 57 and 57+20)) B1,
-     (select avg(ss_list_price) B2_LP
-            ,count(ss_list_price) B2_CNT
-            ,count(distinct ss_list_price) B2_CNTD
-      from store_sales
-      where ss_quantity between 6 and 10
-        and (ss_list_price between 90 and 90+10
-          or ss_coupon_amt between 2323 and 2323+1000
-          or ss_wholesale_cost between 31 and 31+20)) B2,
-     (select avg(ss_list_price) B3_LP
-            ,count(ss_list_price) B3_CNT
-            ,count(distinct ss_list_price) B3_CNTD
-      from store_sales
-      where ss_quantity between 11 and 15
-        and (ss_list_price between 142 and 142+10
-          or ss_coupon_amt between 12214 and 12214+1000
-          or ss_wholesale_cost between 79 and 79+20)) B3,
-     (select avg(ss_list_price) B4_LP
-            ,count(ss_list_price) B4_CNT
-            ,count(distinct ss_list_price) B4_CNTD
-      from store_sales
-      where ss_quantity between 16 and 20
-        and (ss_list_price between 135 and 135+10
-          or ss_coupon_amt between 6071 and 6071+1000
-          or ss_wholesale_cost between 38 and 38+20)) B4,
-     (select avg(ss_list_price) B5_LP
-            ,count(ss_list_price) B5_CNT
-            ,count(distinct ss_list_price) B5_CNTD
-      from store_sales
-      where ss_quantity between 21 and 25
-        and (ss_list_price between 122 and 122+10
-          or ss_coupon_amt between 836 and 836+1000
-          or ss_wholesale_cost between 17 and 17+20)) B5,
-     (select avg(ss_list_price) B6_LP
-            ,count(ss_list_price) B6_CNT
-            ,count(distinct ss_list_price) B6_CNTD
-      from store_sales
-      where ss_quantity between 26 and 30
-        and (ss_list_price between 154 and 154+10
-          or ss_coupon_amt between 7326 and 7326+1000
-          or ss_wholesale_cost between 7 and 7+20)) B6
- limit 100;""", engine)
-df.rename(columns=str.lower, inplace=True)
-st.dataframe(df)
-
 
 st.write("query 29")
 
@@ -238,8 +174,7 @@ ax.set_xlabel('Product')
 ax.set_ylabel('Purchase Return')
 ax.legend()
 
-# rotate the y-axis label
-ax.tick_params(axis='y', labelrotation=0)
+
 
 # display the chart in Streamlit
 st.pyplot(fig)
