c5d32524689fbe3aaaf079143b24db89ee9e496b
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-18 14:12:33 -0400

    Update 3_TPCDS_dataset.py

--- pages/3_TPCDS_dataset.py
+++ pages/3_TPCDS_dataset.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 
 #trying chemy
 engine = create_engine(URL(
-    account = 'wh80921.us-east-2.aws',
-    user = 'clarison',
-    password = '23301631cD*',
+    account = 'xfb32314.us-east-1',
+    user = 'ALEKYAKASTURY',
+    password = '@Noon1240',
     database = 'SNOWFLAKE_SAMPLE_DATA',
     schema = 'TPCDS_SF10TCL',
     warehouse = 'compute_wh'
