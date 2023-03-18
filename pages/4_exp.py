b04d3d40bf13371516491915d6cf0bbc9ba4ec66
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-03-18 11:10:40 -0400

    Update 4_exp.py

--- pages/4_exp.py
+++ pages/4_exp.py
@@ -168,7 +168,7 @@ where i_category in ('{category}'))
         union all
         select * from ws) tmp1
  group by manufacturer
- order by total_sales
+ order by total_sales,manufacturer
  limit 10;"""
 
 
