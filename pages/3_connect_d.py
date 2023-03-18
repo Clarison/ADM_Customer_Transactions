514f753db8312e1c99d0f50743abce1a52ce0f7c
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-17 21:29:56 -0400

    Update 3_connect_d.py

--- pages/3_connect_d.py
+++ pages/3_connect_d.py
@@ -178,42 +178,7 @@ ax.legend()
 # display the chart in Streamlit
 st.pyplot(fig)
 
-st.write("query 34")
-
-df = pd.read_sql_query("""select c_last_name
-       ,c_first_name
-       ,c_salutation
-       ,c_preferred_cust_flag
-       ,ss_ticket_number
-       ,cnt from
-   (select ss_ticket_number
-          ,ss_customer_sk
-          ,count(*) cnt
-    from store_sales,date_dim,store,household_demographics
-    where store_sales.ss_sold_date_sk = date_dim.d_date_sk
-    and store_sales.ss_store_sk = store.s_store_sk  
-    and store_sales.ss_hdemo_sk = household_demographics.hd_demo_sk
-    and (date_dim.d_dom between 1 and 3 or date_dim.d_dom between 25 and 28)
-    and (household_demographics.hd_buy_potential = '>10000' or
-         household_demographics.hd_buy_potential = 'Unknown')
-    and household_demographics.hd_vehicle_count > 0
-    and (case when household_demographics.hd_vehicle_count > 0 
-	then household_demographics.hd_dep_count/ household_demographics.hd_vehicle_count 
-	else null 
-	end)  > 1.2
-    and date_dim.d_year in (1999,1999+1,1999+2)
-    and store.s_county in ('Williamson County','Williamson County','Williamson County','Williamson County',
-                           'Williamson County','Williamson County','Williamson County','Williamson County')
-    group by ss_ticket_number,ss_customer_sk) dn,customer
-    where ss_customer_sk = c_customer_sk
-      and cnt between 15 and 20
-    order by c_last_name,c_first_name,c_salutation,c_preferred_cust_flag desc, ss_ticket_number;""", engine)
-df.rename(columns=str.lower, inplace=True)
-st.dataframe(df)
-
-
-# Connect to the database and get the list of distinct counties from the store table
-distinct_counties = pd.read_sql_query('SELECT DISTINCT s_county FROM store', engine)['s_county'].tolist()
+st.write("Display all customers with specific buy potentials and whose dependent count to vehicle count ratio is larger than 1.2, who in three consecutive years made purchases with between 15 and 20 items in the beginning or the end of each month in stores located in 8 counties.")
 
 # Create a sidebar with a county parameter input widget
 distinct_counties = pd.read_sql_query('SELECT DISTINCT s_county FROM store', engine)['s_county'].tolist()
