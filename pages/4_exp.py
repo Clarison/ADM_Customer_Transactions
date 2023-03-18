9dd6a9244985967e8c989929736ce7f048eea9bd
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-03-17 20:24:54 -0400

    Update 4_exp.py

--- pages/4_exp.py
+++ pages/4_exp.py
@@ -57,7 +57,7 @@ df = pd.read_sql_query("""select  i_item_id,
        cd_marital_status = 'S' and
        cd_education_status = 'College' and
        (p_channel_email = 'N' or p_channel_event = 'N') and
-       d_year = 2000 group by i_item_id order by i_item_id limit 100""", engine, params=(year))
+       d_year = 2000 group by i_item_id order by i_item_id limit 100""", engine)
 df.rename(columns=str.lower, inplace=True)
 st.dataframe(df)
 
