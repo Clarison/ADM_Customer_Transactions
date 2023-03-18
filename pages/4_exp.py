3863eb44a5acb87098a2ac359d1b5c1a34d178f4
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-03-18 11:30:57 -0400

    Update 4_exp.py

--- pages/4_exp.py
+++ pages/4_exp.py
@@ -82,94 +82,49 @@ ax.legend()
 # display the chart in Streamlit
 st.pyplot(fig)
 
-st.write("""What is the monthly sales figure based on extended price for a specific month in a specific year, for
-manufacturers in a specific category in a given time zone. Group sales by manufacturer identifier and sort
-output by sales amount, by channel, and give Total sales.
-""")
+st.write("""Find customers and their detailed customer data who have returned items, which they bought on the web, for an
+amount that is 20% higher than the average amount a customer returns in a given state in a given time period
+across all items. Order the output by customer data.""")
 
 # get user input for year
-
-# get user input for month and year and offset
-ca_gmt_offset = st.number_input('Enter the offset', min_value=-10, max_value=-5)
-
 year = st.number_input('Enter a year', min_value=1998, max_value=2023)
-month = st.number_input('Enter a month', min_value=1, max_value=12)
 
-d_category = pd.read_sql_query("select i_category from item", engine)['i_category'].unique()
-category = st.selectbox('Category', d_category)
+# get user state
+distinct_state_query = "select distinct ca_state from customer_address;"
+distinct_state = pd.read_sql_query(distinct_state_query, engine)['ca_state'].tolist()
 
-query1=f"""with ss as (
- select
-          i_manufact_id,sum(ss_ext_sales_price) total_sales 
- from
- store_sales,
- date_dim,
-         customer_address,
-         item
- where
-         i_manufact_id in (select
-  i_manufact_id
-from
- item
-where i_category in ('{category}'))
- and     ss_item_sk              = i_item_sk
- and     ss_sold_date_sk         = d_date_sk
- and     d_year                  = {year}
- and     d_moy                   = {month}
- and     ss_addr_sk              = ca_address_sk
- and     ca_gmt_offset           = {ca_gmt_offset}
- group by i_manufact_id),
- cs as (
- select
-          i_manufact_id,sum(cs_ext_sales_price) total_sales
- from
- catalog_sales,
- date_dim,
-         customer_address,
-         item
- where
-         i_manufact_id               in (select
-  i_manufact_id
-from
- item
-where i_category in ('{category}'))
- and     cs_item_sk              = i_item_sk
- and     cs_sold_date_sk         = d_date_sk
- and     d_year                  = {year}
- and     d_moy                   = {month}
- and     cs_bill_addr_sk         = ca_address_sk
- and     ca_gmt_offset           = {ca_gmt_offset}
- group by i_manufact_id),
- ws as (
- select
-          i_manufact_id,sum(ws_ext_sales_price) total_sales
- from
- web_sales,
- date_dim,
-         customer_address,
-         item
- where
-         i_manufact_id               in (select
-  i_manufact_id
-from
- item
-where i_category in ('{category}'))
- and     ws_item_sk              = i_item_sk
- and     ws_sold_date_sk         = d_date_sk
- and     d_year                  = {year}
- and     d_moy                   = {month}
- and     ws_bill_addr_sk         = ca_address_sk
- and     ca_gmt_offset           = {ca_gmt_offset}
- group by i_manufact_id)
-  select  i_manufact_id ,sum(total_sales) total_sales
- from  (select * from ss 
-        union all
-        select * from cs 
-        union all
-        select * from ws) tmp1
- group by i_manufact_id
- order by total_sales
- limit 10;"""
+# create a dropdown for the education status parameter with the distinct education status values
+state = st.selectbox('Education Status', distinct_state)
+
+query1=f"""with customer_total_return as
+ (select wr_returning_customer_sk as ctr_customer_sk
+        ,ca_state as ctr_state, 
+ sum(wr_return_amt) as ctr_total_return
+ from web_returns
+     ,date_dim
+     ,customer_address
+ where wr_returned_date_sk = d_date_sk 
+   and d_year ={year}
+   and wr_returning_addr_sk = ca_address_sk 
+ group by wr_returning_customer_sk
+         ,ca_state)
+  select  c_customer_id,c_salutation,c_first_name,c_last_name,c_preferred_cust_flag
+       ,c_birth_day,c_birth_month,c_birth_year,c_birth_country,c_login,c_email_address
+       ,c_last_review_date,ctr_total_return
+ from customer_total_return ctr1
+     ,customer_address
+     ,customer
+ where ctr1.ctr_total_return > (select avg(ctr_total_return)*1.2
+ from customer_total_return ctr2 
+                  where ctr1.ctr_state = ctr2.ctr_state)
+       and ca_address_sk = c_current_addr_sk
+       and ca_state = '{state}'
+       and ctr1.ctr_customer_sk = c_customer_sk
+ order by c_customer_id,c_salutation,c_first_name,c_last_name,c_preferred_cust_flag
+                  ,c_birth_day,c_birth_month,c_birth_year,c_birth_country,c_login,c_email_address
+                  ,c_last_review_date,ctr_total_return
+ limit 100;
+"""
 
 
 df1 = pd.read_sql_query(query1, engine)
