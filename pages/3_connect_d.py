6e85898e2d4fb07144d429063b67a5169926805e
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-17 20:48:07 -0400

    Update 3_connect_d.py

--- pages/3_connect_d.py
+++ pages/3_connect_d.py
@@ -90,7 +90,8 @@ df = pd.read_sql_query(query, engine)
 # rename the columns to lowercase
 df.rename(columns=str.lower, inplace=True)
 
-
+# sort the dataframe by agg2 in descending order
+df= df.sort_values('agg2', ascending=False)
 
 # create a bar chart
 fig, ax = plt.subplots()
